f901f4665446e0e18a19c881fe70ec3789d8188d
foysal41 <mdfoysal.mf33@gmail.com>
2026-08-24 18:22:20 +0600

    added login and register page and setup with mongodb and better auth

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,17 @@
+import dns from "node:dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error("MONGODB_URI is not defined");
+}
+
+const client = new MongoClient(uri);
+
+const clientPromise = client.connect();
+
+export default clientPromise;
